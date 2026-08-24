2a9173631d59f272a7cc82a10a73852cf959969b
Nice <hoangkimc93@gmail.com>
2026-08-24 23:17:00 +0700

    chore: move var to html_vars instead

--- src/CharacterThread.js
+++ src/CharacterThread.js
@@ -187,8 +187,6 @@ async function make_game(proc_args) {
   game_context.Local = "";
   game_context.Dev = "";
   game_context.Place = "code";
-  game_context.last_deploy = "";
-  game_context.is_tauri = "";
   game_context.server_address = "wss://" + proc_args.realm_address;
   game_context.server_path = proc_args.realm_path;
   game_context.server_port = proc_args.realm_port;
