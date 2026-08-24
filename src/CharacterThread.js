96408338309101796146f57a70c6ba3900840454
Nice <hoangkimc93@gmail.com>
2026-08-24 23:06:26 +0700

    chore(tauri): up to date with new release

--- src/CharacterThread.js
+++ src/CharacterThread.js
@@ -246,14 +246,14 @@ async function make_game(proc_args) {
   game_context.Local = "";
   game_context.Dev = "";
   game_context.Place = "code";
+  game_context.last_deploy = "";
+  game_context.is_tauri = "";
   game_context.server_address = "wss://" + proc_args.realm_address;
   game_context.server_path = proc_args.realm_path;
   game_context.server_port = proc_args.realm_port;
   game_context.user_id = proc_args.sess.split("-")[0];
   game_context.user_auth = proc_args.sess.split("-")[1];
   game_context.character_to_load = proc_args.cid;
-  game_context.last_deploy = "";
-  game_context.is_tauri = "";
 
   //expose the block under parent.caracAL
   const extensions = {};
